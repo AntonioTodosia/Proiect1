6e50a15b8d66a73acbee81232032c96f5e7ed211
Todosia Antonio <todosiaantonio@gmail.com>
2024-03-12 21:24:24 +0200

    Add colapsse ...

--- app/components/sectiunea2.jsx
+++ app/components/sectiunea2.jsx
@@ -62,12 +62,22 @@ function Section2(){
                             placeholder="blur" 
                             />
                         </div>
-                        <div className='col-6'>
+                        <div className='col-lg-6 col-sm-12'>
                         <h4 className='py-5'>Text din carte</h4>
-                            <p>Lui Moș Ilie i s-au arătat, a fost îndrumat și ajutat în această misiune de a construi Sfânta Raclă în cinstea Sfântului Ioan Botezătorul de către însuși Maica Domnului, Sfântul Ioan Botezătorul, Sfântul Ioan Evanghelistul, Sfântul Nicolae care s-a rugat pentru această lucrare, Sfântul Apostol Pavel și Sfânta Parascheva, dealtfel a fost ajtat de toți sfinții pomeniți, în toată lucrarea și sarcina pe care a avut-o de îndeplinit prin poruncă Dumnezeiască, Prin Moș Ilie. Proorocul din Hurez, Proorocul românilor, a arătat Dumnezeu cum să fie pregătită cea de a doua Venire a Mântuitorului nostru Iisus Hristos, aici la noi, în grădina Maicii Domnului.
-                                La începutul lunii decembrie, din vorbă în vorbă s-a aflat că Părintele Pantelimon împreună cu încă trei persoane, trebuie să sosească cu Sfânta Raclă de la București și va fi sfințită pe 6 Dececembrie de Sfântul Nicolae. 
-                                Seara dianinte, pe 5 decembrie, trebuia să sosească cu Racla în gară, în Făgăraș. Credincioșii din Hurez împreună cu preoți și călugări din mănăstirea Sâmbăta de Sus, s-au dus la gară să-l aștepte pe părintele Pantelimon, care era însoțit de ginerele lui Moș Ilie, un bărbat, Nelu îl chema, din satul Voievodeni și un bărbat din Întorsura Buzăului. Vreo 200 de oameni așteptau în gară, iar până să sosească părintele Pantelimon împreună cu cei trei bărbați, securitatea i-a arestat pe toți. Preoți și civili, nu i-au ținut mult și le-au dat drumul, între ei au fost și oameni ai securității, îmbrăcați în preoți.
-                                Atât la București, cât și la Făgăraș, erau  în toate gările oameni de la securitate. Cu racla de 1,80 m lungime nu era ușor să treci neobservat pe peronul gării. La îndemnul Părintelui Arsenie Boca ( Sfântul Ardealului), deoarece erau urmăriți, nu au reșit să urce în trenul cu care și-au propus.Mănăstirea PACEA LUMII din Bucium încă nu s-a construit. Aceasta este un dar și i se cere poporului român să o facă, dacă nu o va face, se va lua darul de la poporul român și i se va da altui neam mai vrednic. Să nu lăsăm să ni se ia acest dar.</p>
+                        <p>Lui Moș Ilie i s-au arătat, a fost îndrumat și ajutat în această misiune de a construi Sfânta Raclă în cinstea Sfântului Ioan Botezătorul de către însuși Maica Domnului, Sfântul Ioan Botezătorul, Sfântul Ioan Evanghelistul, Sfântul Nicolae care s-a rugat pentru această lucrare, Sfântul Apostol Pavel și Sfânta Parascheva, dealtfel a fost ajtat de toți sfinții pomeniți, în toată lucrarea și sarcina pe care a avut-o de îndeplinit prin poruncă Dumnezeiască, Prin Moș Ilie.
+                            <button class="btn btn-secondary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample" aria-expanded="false" aria-controls="collapseWidthExample">
+                                Citește mai mult
+                            </button>
+                        </p>
+                        <div style={{minHeight: "120px;"}}>
+                            <div class="collapse collapse-horizontal" id="collapseWidthExample">
+                                <div class="card card-body" style={{width: "auto"}}> 
+                                Proorocul din Hurez, Proorocul românilor, a arătat Dumnezeu cum să fie pregătită cea de a doua Venire a Mântuitorului nostru Iisus Hristos, aici la noi, în grădina Maicii Domnului.
+                                La începutul lunii decembrie, din vorbă în vorbă s-a aflat că Părintele Pantelimon împreună cu încă trei persoane, trebuie să sosească cu Sfânta Raclă de la București și va fi sfințită pe 6 Dececembrie de Sfântul Nicolae.Seara dianinte, pe 5 decembrie, trebuia să sosească cu Racla în gară, în Făgăraș. Credincioșii din Hurez împreună cu preoți și călugări din mănăstirea Sâmbăta de Sus, s-au dus la gară să-l aștepte pe părintele Pantelimon, care era însoțit de ginerele lui Moș Ilie, un bărbat, Nelu îl chema, din satul Voievodeni și un bărbat din Întorsura Buzăului. Vreo 200 de oameni așteptau în gară, iar până să sosească părintele Pantelimon împreună cu cei trei bărbați, securitatea i-a arestat pe toți. Preoți și civili, nu i-au ținut mult și le-au dat drumul, între ei au fost și oameni ai securității, îmbrăcați în preoți.
+                                Atât la București, cât și la Făgăraș, erau  în toate gările oameni de la securitate. Cu racla de 1,80 m lungime nu era ușor să treci neobservat pe peronul gării. La îndemnul Părintelui Arsenie Boca ( Sfântul Ardealului), deoarece erau urmăriți, nu au reșit să urce în trenul cu care și-au propus.Mănăstirea PACEA LUMII din Bucium încă nu s-a construit. Aceasta este un dar și i se cere poporului român să o facă, dacă nu o va face, se va lua darul de la poporul român și i se va da altui neam mai vrednic. Să nu lăsăm să ni se ia acest dar.
+                                </div>
+                            </div> 
+                        </div>
                         </div>
                     </div>
                 </div>
--- app/components/sectiunea3.jsx
+++ app/components/sectiunea3.jsx
@@ -12,7 +12,7 @@ function Section3(){
                     <iframe className="map embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d17505.08580351811!2d22.899018649825788!3d47.07125834771361!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x474860fea97255e7%3A0x941a575088897395!2sBiserica%20Na%C8%99terea%20Sf%C3%A2ntului%20Ioan%20Botez%C4%83torul!5e0!3m2!1sro!2sro!4v1709928095628!5m2!1sro!2sro" width="100%" height="600" ></iframe>                
                 </div>
             </div>
-            <div className='container'>
+            <div className='container1 contacte'>
                 <div className='row py-5'>
                     <div className='col-lg-6'>
                     <Image 
